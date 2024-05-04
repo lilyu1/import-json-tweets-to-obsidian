38788ceb16b3dc7c969bc5667db672efae7f3a31
Lily Ung <omgiluhbu@gmail.com>
2024-05-04 20:25:54 +1000

    initial upload

---
+++ README.md
@@ -0,0 +1,21 @@
+# Convert tweets exported from twitter-web-exporter to .MD files for Obsidian
+
+This currently is tested for bookmarks page.
+Many thanks to contributers of twitter-web-exporter <3
+
+## Output
+
+![Example image](example.png)
+
+## Requirements
+- JSON file from [twitter-web-exporter](https://github.com/prinsss/twitter-web-exporter) containing bookmarked tweets (no need to check 'include all metadata'). You can get this from Export Data option.
+- Python
+
+## Usage
+In CMD enter:
+`python script.py path\to\JSON`
+
+## To do/may do
+- threads??
+- likes??
+- maybe turn into a plugin??
---
+++ script.py
@@ -0,0 +1,42 @@
+import json
+import os
+import sys
+from datetime import datetime
+
+def json_to_md(json_file, output_dir):
+    with open(json_file, 'r') as file:
+        data = json.load(file)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    for tweet in data:
+        tweet_id = tweet['id']
+        md_file = os.path.join(output_dir, f"tweet-{tweet_id}.md")
+        with open(md_file, 'w') as file:
+            # Format the date
+            created_at = tweet['created_at']
+            date_obj = datetime.strptime(created_at, '%Y-%m-%d %H:%M:%S %z')
+            formatted_date = date_obj.strftime('%Y-%m-%d %H:%M')
+
+            # Write tweet content
+            file.write(tweet['full_text'] + "\n\n")
+
+
+            # Write metadata
+            file.write("## Metadata\n")
+            file.write(f"- Author: [{tweet['name']}](https://twitter.com/{tweet['screen_name']})\n")
+            file.write(f"- Username: @{tweet['screen_name']}\n")
+            file.write(f"- Date: {formatted_date}\n")
+            file.write(f"- URL: [Link]({tweet['url']})\n")
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <json_file>")
+        sys.exit(1)
+
+    json_file = sys.argv[1]
+    output_dir = 'tweets'
+
+    json_to_md(json_file, output_dir)
+    print(f"Markdown files generated in the '{output_dir}' folder.")
